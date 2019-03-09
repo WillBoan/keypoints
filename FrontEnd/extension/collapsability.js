960ddf7f621cb5720c85392238296a1aa1df9d52
Will Boan <waboan@gmail.com>
2019-03-09 10:37:55 -0500

    Improvements

--- FrontEnd/extension/collapsability.js
+++ FrontEnd/extension/collapsability.js
@@ -1,361 +1,6 @@
 $(function() {
 
 
-  // MAKE THE HTML SENSIBLE/REMOVE USELESS STUFF:
-  // Remove section divs (unwrapping their contents):
-  const section_divs = $("[class^='Section']");
-  section_divs.children().unwrap();
-  // Remove spacers:
-  const spacers = $(".MsoNormal").filter(function() {
-    return /^\s+$/i.test($(this).text());
-  });
-  spacers.remove();
-  // Remove useless spans:
-  const useless_spans = $(".documentcontent,.WordSection1").children().filter(function() {
-    return $(this).text().trim().length === 0;
-  });
-  useless_spans.remove();
-  // Remove explicit attributes (margin-left and text-indent):
-  $(".documentcontent").find("*").css({"margin-left":"", "text-indent":""});
-  // Fix para indents:
-  const indent_spans = $("span[style='font:7.0pt \"Times New Roman\"']");
-  const new_indent = "\u2003".repeat(2);
-  indent_spans.each(function(index) {
-    $(this).text(new_indent);
-  });
-
-
-  // PARA AND PARA ANCHOR ELEMENTS:
-  // Array of DOM paragraph number elements:
-  const para_anchors = $(".paragAnchor");
-  // Array of DOM paragraph elements:
-  // ----(Assumes that the paragraphs's element
-  // ----is the closest ancestor with the 'p' tag)
-  const para_elements = para_anchors.closest("p");
-  para_elements.addClass("kp-para");
-  // Function to find the paragraph's element:
-  // ----(Based on para_elements)
-  function getParaElement(para_num) {
-    return para_elements.eq(para_num - 1);
-  }
-  // Function to get para_num from para element:
-  function getParaNum(para_element) {
-    return para_num = Number($(para_element).find(".paragAnchor").text());
-  }
-  // Get last para_num:
-  let last_para_num = getParaNum(para_elements.last());
-  // Remove elements after last para:
-  para_elements.last().nextAll().remove();
-
-
-  // $("style").remove();
-  // let stylesheets = $("[rel='stylesheet']");
-  // console.log(`${stylesheets.length} stylesheets`);
-  // stylesheets.eq(6).remove();
-  // $("[href='https://cdn1.canlii.org/css/A.canlii-global.css.pagespeed.cf.GWYYTlf3fV.css']").remove();
-
-
-
-
-
-
-
-  // CITATION DATA:
-  // Test input; represents cited paragraph numbers
-  const unindexed_citation_data = [
-    {para:10, citing_case:"2008 FC 991", citing_para:21},
-    {para:12, citing_case:"2008 FC 991", citing_para:22},
-    {para:50, citing_case:"2008 FC 991", citing_para:37},
-    {para:12, citing_case:"2009 SCC 12", citing_para:4},
-    {para:9, citing_case:"2016 FCA 93", citing_para:49},
-    {para:18, citing_case:"2011 SCC 61", citing_para:41},
-    {para:36, citing_case:"2010 BCCA 191", citing_para:34}
-  ];
-  // Reorganize the unindexed_citation_data, indexing by para number:
-  // ----The final data structure should end up in this format:
-  // ----citation_data = {
-  // ----  10: [{citing_case:"2008 FC 991", citing_para:21}],
-  // ----  12: [
-  // ----    {citing_case:"2008 FC 991", citing_para:22},
-  // ----    {citing_case:"2009 SCC 12", citing_para:4}
-  // ----  ]
-  // ----};
-  let citation_data = {};
-  unindexed_citation_data.forEach((x) => {
-    citation_data[x.para] = (citation_data[x.para] || []);
-    citation_data[x.para].push(
-      {citing_case:x.citing_case,
-      citing_para:x.citing_para}
-    );
-  });
-
-
-  // CITATION COUNTS:
-  function getCitationCount(para_num) {
-    return citation_data[para_num].length;
-  }
-  // Const for how many times the most cited para has been cited:
-  const highest_cit_count = Math.max(...Object.values(citation_data).map((x) => x.length));
-  // Function to calculate the proportion of
-  // how many times a paragraph has been
-  // cited relative to the most cited paragraph:
-  function calcCountProportion(para_num) {
-    return getCitationCount(para_num) / highest_cit_count;
-  }
-
-
-  // PARAGRAPH COLORING:
-  const darkest_color = [225, 100, 0];
-  const lightest_color = [255, 255, 255];
-  // Function that calculates an RGB component value:
-  function calcColorComponent(n, count_proportion) {
-    return lightest_color[n] - ((lightest_color[n] - darkest_color[n]) * count_proportion);
-  }
-  // Function that calculates the highlight color for a paragraph,
-  // based on how much the paragraph has been cited:
-  function calcHighlightColor(para_num) {
-    let count_proportion = calcCountProportion(para_num);
-    let color = [
-      calcColorComponent(0, count_proportion),
-      calcColorComponent(1, count_proportion),
-      calcColorComponent(2, count_proportion)
-    ];
-    let final_color = `rgb(${color[0]}, ${color[1]}, ${color[2]})`;
-    return final_color;
-  }
-  // Main function to apply highlight background color:
-  // ----(Also writes the highlight_color to modified_paras)
-  function applyHighlight(para_num, para) {
-    let highlight_color = calcHighlightColor(para_num);
-    modified_paras[para_num] = {highlight_color: highlight_color};
-    $(para).css("background-color", highlight_color);
-  }
-
-
-  // LINKS:
-  // Const
-  const jurisdiction_map = {
-    // Federal:
-    scc: {court_url_id:"scc", jur:"ca"},
-    fca: {court_url_id:"fca", jur:"ca"},
-    fc: {court_url_id:"fct", jur:"ca"}, // Weird one: fc vs fct
-    tcc: {court_url_id:"tcc", jur:"ca"},
-    // BC:
-    bcca: {court_url_id:"bcca", jur:"bc"},
-    bcsc: {court_url_id:"bcsc", jur:"bc"},
-    bcpc: {court_url_id:"bcpc", jur:"bc"},
-    // AB:
-    abca: {court_url_id:"abca", jur:"ab"},
-    abqb: {court_url_id:"abqb", jur:"ab"},
-    abpc: {court_url_id:"abpc", jur:"ab"},
-    // SK:
-    skca: {court_url_id:"skca", jur:"sk"},
-    skqb: {court_url_id:"skqb", jur:"sk"},
-    skpc: {court_url_id:"skpc", jur:"sk"},
-    skdc: {court_url_id:"skdc", jur:"sk"}, // NOTE: 1983 CanLII 2300 (SKDC) would be an ID
-    skufc: {court_url_id:"skufc", jur:"sk"}, // NOTE: 1983 CanLII 2405 (SK UFC) would be an ID
-    // MB:
-    mbca: {court_url_id:"mbca", jur:"mb"},
-    mbqb: {court_url_id:"mbqb", jur:"mb"},
-    mbpc: {court_url_id:"mbpc", jur:"mb"},
-    // ON:
-    onca: {court_url_id:"onca", jur:"on"},
-    onsc: {court_url_id:"onsc", jur:"on"},
-    onscdc: {court_url_id:"onscdc", jur:"on"}, // https://www.canlii.org/en/on/onscdc/doc/2018/2018onsc484/2018onsc484.html
-    onscsm: {court_url_id:"onscsm", jur:"on"}, // Weird one
-    oncj: {court_url_id:"oncj", jur:"on"},
-    // QC:
-    qcca: {court_url_id:"qcca", jur:"qc"},
-    qccs: {court_url_id:"qccs", jur:"qc"},
-    qccq: {court_url_id:"qccq", jur:"qc"},
-    qctdp: {court_url_id:"qctdp", jur:"qc"},
-    qctp: {court_url_id:"qctp", jur:"qc"},
-    qccm: {court_url_id:"qccm", jur:"qc"}, // Weird one
-    // NB:
-    nbca: {court_url_id:"nbca", jur:"nb"},
-    nbqb: {court_url_id:"nbqb", jur:"nb"},
-    nbpc: {court_url_id:"nbpc", jur:"nb"},
-    // NS:
-    nsca: {court_url_id:"nsca", jur:"ns"},
-    nssc: {court_url_id:"nssc", jur:"ns"},
-    nssf: {court_url_id:"nssf", jur:"ns"},
-    nspc: {court_url_id:"nspc", jur:"ns"},
-    nssm: {court_url_id:"nssm", jur:"ns"},
-    nspb: {court_url_id:"nspr", jur:"ns"}, // Weird one: nspb vs nspr
-    nsfc: {court_url_id:"nsfc", jur:"ns"},
-    // PE:
-    peca: {court_url_id:"pescad", jur:"pe"}, // Weird one: peca vs pescad
-    pesc: {court_url_id:"pesctd", jur:"pe"}, // Weird one: pesc vs pesctd
-    // NL:
-    nlca: {court_url_id:"nlca", jur:"nl"},
-    nlsc: {court_url_id:"nlsc", jur:"nl"},
-    nlpc: {court_url_id:"nlpc", jur:"nl"}, // Weird one (CanLII)
-    nlma: {court_url_id:"nlma", jur:"nl"},
-    // YK:
-    ykca: {court_url_id:"ykca", jur:"yk"},
-    yksc: {court_url_id:"yksc", jur:"yk"},
-    yktc: {court_url_id:"yktc", jur:"yk"},
-    yksm: {court_url_id:"yksm", jur:"yk"},
-    // NT:
-    nwtca: {court_url_id:"ntca", jur:"nt"}, // Weird one: nwtca vs ntca
-    nwtsc: {court_url_id:"ntsc", jur:"nt"}, // Weird one: nwtsc vs ntsc
-    nwttc: {court_url_id:"nttc", jur:"nt"}, // Weird one: nwttc vs nttc
-    // nwttc: {court_url_id:"ntyjc", jur:"nt"}, // Weird one: nwttc vs ntyjc; NOTE: same key as previous
-    // NU:
-    nuca: {court_url_id:"nuca", jur:"nu"},
-    nucj: {court_url_id:"nucj", jur:"nu"},
-    yjcn: {court_url_id:"nuyc", jur:"nu"}, // Weird one: yjcn vs nuyc
-  };
-  const base_url = "https://www.canlii.org/en/";
-  // Function to get url from case ID:
-  function getUrl(case_id) {
-    let id_nospace = case_id.replace(/ /g, "").toLowerCase();
-    let id_split = case_id.toLowerCase().split(" ");
-    let year = id_split[0];
-    let court = id_split[1];
-    let num = id_split[2];
-    let court_url_id = jurisdiction_map[court].court_url_id;
-    let jur = jurisdiction_map[court].jur;
-    let url = base_url + `${jur}/${court_url_id}/doc/${year}/${id_nospace}/${id_nospace}.html`;
-    return url;
-  }
-  // Function to create links for all of the citations for a paragraph,
-  // and append the links to the end of the paragraphs:
-  function createAppendLinks(para_num, para) {
-    // Create an <a> element for each link:
-    let citing_cases_html = citation_data[para_num].map((x) => {
-      let link_text = x.citing_case + " ¶" + x.citing_para;
-      let link_url = getUrl(x.citing_case) + `#par${x.citing_para}`;
-      let link_html = `<a href=${link_url}>${link_text}</a>`;
-      return link_html;
-    }).join("</br>");
-    // Wrap these <a> elements in two <divs>:
-    let final_html =
-      `<div class="kp-citing-case-link-cont">
-        <div class="kp-citing-case-link">
-          ${citing_cases_html}
-        </div>
-      </div>`;
-    // Append the links to the para element:
-    $(para).append(final_html);
-  }
-
-
-
-
-
-  // MAIN PARAGRAPH MODIFICATION:
-  // Object for modified para properties:
-  // ----This is so that later we can get properties
-  // ----(eg highlight color) from para numbers
-  modified_paras = {};
-  // Highlight and create/append links for the cited paragraphs:
-  Object.keys(citation_data).forEach((para_num) => {
-
-    // Get parent element:
-    let para = getParaElement(para_num);
-    // Attach the para_num within the para element, and add
-    // a class to indicate that the para has been modified:
-    $(para).attr("data-para-num", para_num);
-    $(para).addClass("kp-modified-para");
-
-    // Apply highlight color:
-    applyHighlight(para_num, para);
-
-    // Create and append the links:
-    createAppendLinks(para_num, para);
-  });
-
-
-  // MOUSEOVER LOGIC FOR CITED PARAS:
-  const mouseover_color = "rgb(234, 234, 234)";
-  // Hide and set mouseover color for the citing case links:
-  $(".kp-citing-case-link").hide();
-  $(".kp-citing-case-link").css("background-color", mouseover_color);
-  // Mouseover functions:
-  $(".kp-modified-para").hover(
-    function mouseOver() {
-      $(this).css("background-color", mouseover_color);
-      $(this).find(".kp-citing-case-link").show(150);
-    },
-    function mouseOut() {
-      let para_num = $(this).attr("data-para-num");
-      let highlight_color = modified_paras[para_num].highlight_color;
-      $(this).css("background-color", highlight_color);
-      $(this).find(".kp-citing-case-link").hide(150);
-    }
-  );
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
   // Judge declarations:
   const judge_declaration_regex = /(judge?ment|reason).+delivered\s+by:?\s*$/s;
   const judge_declarations = $("p").filter(function() {
@@ -698,7 +343,7 @@ $(function() {
 
 
   // All headings should start collapsed:
-  collapseAll();
+  // collapseAll();
 
   // Make headings collapse/expand on click:
   headings.click(function() {
@@ -759,52 +404,26 @@ $(function() {
   }
 
   // Add 'expand all' button:
-  // let expand_collapse_button = $("<p></p>")
-  //   .text("Expand all")
-  //   .addClass("kp-expand-all")
-  //   .click(function() {
-  //     if ($(this).text() === "Expand all") {
-  //       expandAll();
-  //       $(this).text("Collapse all");
-  //     } else {
-  //       collapseAll();
-  //       $(this).text("Expand all");
-  //     }
-  //   });
-  // $(".documentcontent").prepend(expand_collapse_button);
   let expand_all_button = $("<p></p>")
     .text("Expand all")
+    .addClass("kp-expand-all")
     .click(function() {
       expandAll();
     });
-  let collapse_all_button = $("<p></p>")
-    .text("Collapse all")
-    .click(function() {
-      collapseAll();
-    });
-  $(".documentcontent").prepend(expand_all_button, collapse_all_button);
+  $(".documentcontent").prepend(expand_all_button);
+
+
+
 
 
 
 
 
   // SCROLLING:
-  // const top_bar_height = 33;
-  // const top_bar_height_minus_margin = 17;
-  // var controller = new ScrollMagic.Controller();
-  // $(document.getElementsByClassName("kp-heading")).each(function() {
-  //   let rank = getRank($(this));
-  //   let element_offset = $(this).offset().top;
-  //   let scene = new ScrollMagic.Scene({
-  //     offset: element_offset - top_bar_height - (rank * 28)
-  //     // duration: 400
-  //   })
-  //   .setPin(this);
-  //   controller.addScene(scene);
-  // });
+  const top_bar_height = 33;
+  const top_bar_height_minus_margin = 17;
 
 
-// OLD SCROLLING WAY 1:
   // $(window).scroll(function() {
   //   let doc_scroll_top = $(document).scrollTop();
   //   console.log(`doc_scroll_top: ${doc_scroll_top}`);
@@ -831,7 +450,22 @@ $(function() {
   // }
   // stickHeading(headings.eq(0));
   // stickHeading(headings.eq(1));
-// OLD SCROLLING WAY 2:
+
+
+
+
+  var controller = new ScrollMagic.Controller();
+  $(document.getElementsByClassName("kp-heading")).each(function() {
+    let rank = getRank($(this));
+    let element_offset = $(this).offset().top;
+    let scene = new ScrollMagic.Scene({
+      offset: element_offset - top_bar_height - (rank * 28)
+      // duration: 400
+    })
+    .setPin(this);
+    controller.addScene(scene);
+  });
+  //
   // function stickHeading(heading) {
   //   let rank = getRank(heading);
   //   let sticky_point = rank * 28 + top_bar_height_minus_margin;
@@ -845,8 +479,6 @@ $(function() {
   // })
   // .setPin(document.getElementsByClassName("kp-heading")[0]);
   // controller.addScene(scene);
-
-
 
 
 
